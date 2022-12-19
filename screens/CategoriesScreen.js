d14df207740208fc5442d9ec3c439120be966839
Rana_awais <z.manzoor@pioneerlogics.com>
2022-12-18 22:34:37 -0800

    diplay items in grid,background color for full app and cascade style

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList, View, StyleSheet } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+//for render categories create saperate function
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+function CategoriesScreen(props) {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    ></FlatList>
+  );
+}
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
