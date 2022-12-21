790b31a321ffa45c937e2e68f4a88c2914b209a9
Rana_awais <z.manzoor@pioneerlogics.com>
2022-12-20 22:54:04 -0800

    display meals by clickin on grid tile

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { MEALS } from "../data/dummy-data";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import MealsItem from "../components/MealsItem";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealsItem) => {
+    return mealsItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealsItem title={itemData.item.title} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen-{catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
